5171d46004ef09fd525f43e8e34e3ecba065efff
Inixio <inixio97@gmail.com>
2021-02-20 12:08:57 +0100

    ADD OTD

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
 
-function sendPhoto() {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`).then(res => {
+function sendPhoto(dateStr) {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}${dateStr ? `&date=${dateStr}` : ''}`).then(res => {
         const {data} = res;
         const url = data.hdurl ? data.hdurl : data.url;
-        const caption = `🚀 ${data.title} 🪐${data.copyright ? `\n\nCopyright: ${data.copyright}` : ''}`;
+        const caption = `${dateStr ? `On this day ${dateStr.split('-')[0]}\n\n` : ''}🚀 ${data.title} 🪐${data.copyright ? `\n\nCopyright: ${data.copyright}` : ''}`;
         if (data.media_type === 'video'){
             bot.sendMessage(`@${process.env.TELEGRAM_CHANNEL_ID}`, `${caption}\n${url}`);
         } else {
@@ -24,3 +24,12 @@ var job = new CronJob('0 21 * * *', function() {
 }, null, true, 'Europe/Madrid');
 
 job.start();
+
+const onThisDayJob = new CronJob('10 12 * * *', () => {
+    const year = Math.floor(Math.random()*20)+1; // Number between 1 and 20
+    const today = new Date();
+    const month = date.getMonth()+1;
+    const day = date.getDate();
+    const date = `200${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+    sendPhoto(date);
+})
